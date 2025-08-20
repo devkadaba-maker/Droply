faedd06a520908f53e4762ba13092e94e7ffef28
Dev Kadaba <devkadaba@macbookair.lan>
2025-08-20 16:57:55 +1000

    fix(api): correct delete and empty-trash routes; add missing delete route; fix signatures and returning(); add dashboard page for build

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,33 @@
 import { ThemeProviderProps } from "next-themes";
-
+import { ImageKitProvider } from "imagekitio-next";
+import {HeroUIProvider} from "@heroui/react"
 export interface ProviderProps{
     children: React.ReactNode, 
     themeProps?: ThemeProviderProps
 }
+const authenticator = async () => {
+try{
+    const response = await fetch("/api/imagekit-auth")
+    const data = await response.json()
+    return data
+}catch(error){
+    console.error("Auth error:",error)
+    throw error
+}
+}
 export function Providers({children, themeProps}: ProviderProps){
     return(
-        <h1>
+        
+            <ImageKitProvider
+                authenticator={authenticator}
+                publicKey={process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY}
+                urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT}
+                >
+            <HeroUIProvider>
             {children}
-        </h1>
+            </HeroUIProvider>
+            </ImageKitProvider>
+            
+        
     )
 }
